29ea8be32939a096dc49bbaa9d5bea2a3059bf90
unknown <marwen.rebai.92@gmail>
2023-03-01 14:48:13 +0100

    bo complet

---
+++ src/main/java/fr/eni/encheres/bo/Enchère.java
@@ -0,0 +1,36 @@
+package fr.eni.encheres.bo;
+
+public class Enchère {
+	private Date dateEnchère;
+	private float montant_enchere;
+	//constructor
+	public Enchère () {
+		
+}
+	public Enchère(Date dateEnchère, float montant_enchere) {
+		super();
+		this.dateEnchère = dateEnchère;
+		this.montant_enchere = montant_enchere;
+	}
+	// getter and setter
+	public Date getDateEnchère() {
+		return dateEnchère;
+	}
+	public void setDateEnchère(Date dateEnchère) {
+		this.dateEnchère = dateEnchère;
+	}
+	public float getMontant_enchere() {
+		return montant_enchere;
+	}
+	public void setMontant_enchere(float montant_enchere) {
+		this.montant_enchere = montant_enchere;
+	}
+	//to string
+	@Override
+	public java.lang.String toString() {
+		return "Enchère [dateEnchère=" + dateEnchère + ", montant_enchere=" + montant_enchere + "]";
+	}
+	
+	}
+
+	
