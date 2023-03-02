61cf8e88364203e285cc3da94985eb74aa1a7350
unknown <marwen.rebai.92@gmail>
2023-03-02 18:42:58 +0100

    FIX - Corretion architecture DynamicWebProject

--- fr/eni/encheres/bo/Enchere.java
+++ fr/eni/encheres/bo/Enchere.java
@@ -2,24 +2,24 @@ package fr.eni.encheres.bo;
 
 import java.sql.Date;
 
-public class Enchère {
-	private Date dateEnchère;
+public class Enchere {
+	private Date dateEnchere;
 	private float montant_enchere;
 	//constructor
-	public Enchère () {
+	public Enchere () {
 		
 }
-	public Enchère(Date dateEnchère, float montant_enchere) {
+	public Enchere(Date dateEnchere, float montant_enchere) {
 		super();
-		this.dateEnchère = dateEnchère;
+		this.dateEnchere = dateEnchere;
 		this.montant_enchere = montant_enchere;
 	}
 	// getter and setter
-	public Date getDateEnchère() {
-		return dateEnchère;
+	public Date getdateEnchere() {
+		return dateEnchere;
 	}
-	public void setDateEnchère(Date dateEnchère) {
-		this.dateEnchère = dateEnchère;
+	public void setDateEnchère(Date dateEnchere) {
+		this.dateEnchere = dateEnchere;
 	}
 	public float getMontant_enchere() {
 		return montant_enchere;
@@ -30,7 +30,7 @@ public class Enchère {
 	//to string
 	@Override
 	public java.lang.String toString() {
-		return "Enchère [dateEnchère=" + dateEnchère + ", montant_enchere=" + montant_enchere + "]";
+		return "Enchère [dateEnchere=" + dateEnchere + ", montant_enchere=" + montant_enchere + "]";
 	}
 	
 	}
