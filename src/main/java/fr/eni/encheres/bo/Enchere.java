231f9f328b04f9b5ccc95ceef607bf18ff687c0d
Junkogetsu <junkogetsu@gmail.com>
2023-03-02 23:07:46 +0100

    UtilisateurDAOJdbcImpl in progress + corrections in other files

---
+++ src/main/java/fr/eni/encheres/bo/Enchere.java
@@ -0,0 +1,36 @@
+package fr.eni.encheres.bo;
+
+public class Enchere {
+	private Date dateEnchere;
+	private float montant_enchere;
+	//constructor
+	public Enchere () {
+		
+}
+	public Enchere(Date dateEnchere, float montant_enchere) {
+		super();
+		this.dateEnchere = dateEnchere;
+		this.montant_enchere = montant_enchere;
+	}
+	// getter and setter
+	public Date getDateEnchere() {
+		return dateEnchere;
+	}
+	public void setDateEnchere(Date dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+	public float getMontant_enchere() {
+		return montant_enchere;
+	}
+	public void setMontant_enchere(float montant_enchere) {
+		this.montant_enchere = montant_enchere;
+	}
+	//to string
+	@Override
+	public java.lang.String toString() {
+		return "Enchere [dateEnchere=" + dateEnchere + ", montant_enchere=" + montant_enchere + "]";
+	}
+	
+	}
+
+	
--- src/main/java/fr/eni/encheres/dal/UtilisateurDAOJdbcImpl.java
+++ src/main/java/fr/eni/encheres/dal/UtilisateurDAOJdbcImpl.java
@@ -1,9 +1,13 @@
 package fr.eni.encheres.dal;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+
+import fr.eni.encheres.bo.Utilisateur;
 
 public class UtilisateurDAOJdbcImpl {
 	
@@ -13,6 +17,8 @@ public class UtilisateurDAOJdbcImpl {
 	
 	// loading connection :
 	
+	private static final String INSERT = "INSERT INTO utilisateurs (pseudo,nom,prenom,email,telephone,rue,codePostal,ville,motDepasse,credit,admnistrateur) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+
 	public UtilisateurDAOJdbcImpl() {
 		  try {
 			  
@@ -26,6 +32,41 @@ public class UtilisateurDAOJdbcImpl {
 		  JdbcTools.closeConnection();
 	}
 	
+	public void insert(Utilisateur utilisateur) throws DALException {
+		
+		try {
+			PreparedStatement ps = JdbcTools.getConnection().prepareStatement(INSERT, PreparedStatement.RETURN_GENERATED_KEYS);
+			fillPreparedStatement(utilisateur, ps);
+			ps.executeUpdate();
+			
+			// récupère l'id
+			ResultSet rs = ps.getGeneratedKeys();
+			if (rs.next()) {
+				utilisateur.setNoUtilisateur(rs.getInt(1));
+			}
+			
+		} catch (SQLException e) {
+			
+		}
+		
+		
+	}
+	
+	// methodes :
+	
+    private void fillPreparedStatement(Utilisateur utilisateur, PreparedStatement ps) throws SQLException {
+    	ps.setString(1, utilisateur.getPseudo());
+        ps.setString(2, utilisateur.getNom());
+        ps.setString(3, utilisateur.getPrenom());
+        ps.setString(4, utilisateur.getEmail());
+        ps.setString(5, utilisateur.getTelephone());
+        ps.setString(6, utilisateur.getRue());
+        ps.setString(7, utilisateur.getCodePostal());
+        ps.setString(8, utilisateur.getVille());
+        ps.setString(9, utilisateur.getMotDePasse());
+        ps.setInt(10, utilisateur.getCredit());
+        ps.setBoolean(11, utilisateur.isAdministrateur());
+    }
 	
 	
 
